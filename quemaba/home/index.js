0846798cce35ebde7721d23b28f9721cb6932a79
enzoAmancio <enzorocha@unipam.edu.br>
2025-06-22 11:32:06 +0000

    atualizando home

--- quemaba/home/index.js
+++ quemaba/home/index.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('.btn-tea');
+/*const buttons = document.querySelectorAll('.btn-tea');
 
 buttons.forEach(button => {
     button.addEventListener('mouseenter', () => {
@@ -10,4 +10,21 @@ buttons.forEach(button => {
         const item = button.closest('.carousel-item');
         if (item) item.classList.remove('blurred');
     });
+});*/
+const botao = document.getElementById('botao-saiba-mais');
+let textoOriginal = 'Saiba mais';
+let textoHover = 'Saiba mais >';
+
+// Troca ao passar o mouse
+botao.addEventListener('mouseenter', () => {
+  botao.textContent = textoHover;
+});
+botao.addEventListener('mouseleave', () => {
+  botao.textContent = textoOriginal;
+});
+
+// Troca ao clicar (mantém "Saiba mais >" após o clique)
+botao.addEventListener('click', () => {
+  botao.textContent = textoHover;
+  window.location.href = 'doacao.html'
 });
